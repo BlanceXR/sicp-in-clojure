360be4c7906518baa7955b779964f42ef09f6bd6
BlanceXR <yizhenwei09@gmail.com>
2013-05-23 20:37:10 -0400

    ex 3.3 & ex 3.4

--- src/sicp/chapter3.clj
+++ src/sicp/chapter3.clj
@@ -30,3 +30,26 @@
       (cond (= :how-many-calls? (first args)) @count
             (= :reset (first args)) (reset! count 0)
             :else (do (swap! count inc) (apply f args))))))
+
+;;; ex 3.3 & ex 3.4
+(defn make-account
+  "make an bank account with password protection. Will call cops if
+  enter 8 consecutive incorrect passwords in a row"
+  [deposit pw]
+  (let [cops-count (atom 0)
+        init-amount (atom deposit)
+        withdraw (fn [amount] (if (>= (- @init-amount amount) 0)
+                                (reset! init-amount (- @init-amount amount))
+                                (throw (Exception. " : insufficent fund"))))
+        deposit (fn [amount] (reset! init-amount (+ @init-amount amount)))]
+    (fn dispatch [op password]
+      (if (= password pw)
+        (do (reset! cops-count 0)
+            (condp = op
+              'withdraw withdraw
+              'deposit deposit
+              (throw (Exception. " : no such operation"))))
+        (do (swap! cops-count inc)
+            (if (> @cops-count 7)
+              (throw (Exception. ": 911!!!!!"))
+              (throw (Exception. ": incorrect password"))))))))
