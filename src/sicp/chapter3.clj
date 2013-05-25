65bfb9d9a6db03abf9ffbdeda2e5572c4a4d7002
BlanceXR <yizhenwei09@gmail.com>
2013-05-24 20:38:02 -0400

    ex 3.7 & 3.8

--- src/sicp/chapter3.clj
+++ src/sicp/chapter3.clj
@@ -35,21 +35,40 @@
 (defn make-account
   "make an bank account with password protection. Will call cops if
   enter 8 consecutive incorrect passwords in a row"
-  [deposit pw]
-  (let [cops-count (atom 0)
+  [deposit first-password]
+  (let [pw (atom #{first-password})
+        cops-count (atom 0)
         init-amount (atom deposit)
         withdraw (fn [amount] (if (>= (- @init-amount amount) 0)
                                 (reset! init-amount (- @init-amount amount))
                                 (throw (Exception. " : insufficent fund"))))
-        deposit (fn [amount] (reset! init-amount (+ @init-amount amount)))]
+        deposit (fn [amount] (reset! init-amount (+ @init-amount amount)))
+        validate (fn [pw-validate] (@pw pw-validate))
+        add-password (fn [new-pw] (do (swap! pw conj new-pw)
+                                      :done))]
     (fn dispatch [op password]
-      (if (= password pw)
+      (if (@pw password)
         (do (reset! cops-count 0)
             (condp = op
               'withdraw withdraw
               'deposit deposit
+              'add-password add-password
               (throw (Exception. " : no such operation"))))
         (do (swap! cops-count inc)
             (if (> @cops-count 7)
               (throw (Exception. ": 911!!!!!"))
               (throw (Exception. ": incorrect password"))))))))
+
+;;; ex 3.7
+(defn make-joint
+  "create a new password for the account"
+  [acc old-pw new-pw]
+  ((acc 'add-password old-pw) new-pw))
+
+;;; ex 3.8
+(defn g [y]
+  (let [y (atom y)]
+    (fn [x] (let [z @y]
+              (reset! y x)
+              z))))
+(def f (g 0))
